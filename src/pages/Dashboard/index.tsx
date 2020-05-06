08b10d2342a2ca937dfdcf3b10a33f9a126536f9
Marcelo Ribeiro Silva <ribmarc@gmail.com>
2020-05-05 22:38:18 -0300

    Conectando a api (em andamento)

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,32 +27,6 @@ const Dashboard: React.FC = () => {
 
           <FiChevronRight size={20} />
         </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/34379943?s=460&u=2c39dd12a2ba294b48eebd095bce34dfa13c0b74&v=4"
-            alt="Marcelo Silva"
-          />
-          <div>
-            <strong>marceloribeirosilva/GoBarber</strong>
-            <p>Aplicativo desenvolvido durante o Bootcamp da RocketSeat</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars1.githubusercontent.com/u/34379943?s=460&u=2c39dd12a2ba294b48eebd095bce34dfa13c0b74&v=4"
-            alt="Marcelo Silva"
-          />
-          <div>
-            <strong>marceloribeirosilva/GoBarber</strong>
-            <p>Aplicativo desenvolvido durante o Bootcamp da RocketSeat</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
       </Repositories>
     </>
   );
